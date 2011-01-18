96d5e5d7843b4824a613f10f365c0328d19cdf8d
Karl Heinz Marbaise <info@soebes.de>
2011-01-18 20:38:29 +0100

    - Added multimodule-deps

---
+++ multimodule-deps/m4/m4.5/src/main/java/com/xyz/testapp/App.java
@@ -0,0 +1,12 @@
+package com.xyz.testapp;
+
+public class App {
+
+	/** Comment added.
+	 * Added more comment.
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is App");
+	}
+
+}
